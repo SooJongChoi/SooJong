1ec51616ba4dc21ab0c2bd6474b6f125bfe0aa5b
SooJongChoi <131238034+SooJongChoi@users.noreply.github.com>
2023-04-23 19:44:09 -0400

    Add files via upload

---
+++ proposal.py
@@ -0,0 +1,50 @@
+# Proposal
+
+## What will (likely) be the title of your project?
+A Python-Based Web Application for Secure Password
+
+TODO
+
+## In just a sentence or two, summarize your project. (E.g., "A website that lets you buy and sell stocks.")
+
+TODO
+I want to make a web that lets people generate password or enter password and let them know if it is a strong or weak password.
+
+## In a paragraph or more, detail your project. What will your software do? What features will it have? How will it be executed?
+
+TODO
+I want to make it that its ability to generate passwords based on user-specified parameters. Users can input their desired length, character set, and other relevant details,
+and the application will generate a password accordingly.
+Additionally, PasswordGenius will also provide feedback on the strength of the
+password based on various criteria such as length, complexity, and randomness.
+
+## If planning to combine 1051's final project with another course's final project, with which other course? And which aspect(s) of your proposed project would relate to 1051, and which aspect(s) would relate to the other course?
+
+TODO, if applicable
+
+## If planning to collaborate with 1 or 2 classmates for the final project, list their names, email addresses, and the names of their assigned TAs below.
+
+TODO, if applicable
+
+## In the world of software, most everything takes longer to implement than you expect. And so it's not uncommon to accomplish less in a fixed amount of time than you hope.
+
+### In a sentence (or list of features), define a GOOD outcome for your final project. I.e., what WILL you accomplish no matter what?
+
+TODO
+A good outcome for the final project would be a functional and secure web application with user-friendly password generation and analysis features.
+I will accomplish the idea of making website and how to debug and make the code work no matter what
+### In a sentence (or list of features), define a BETTER outcome for your final project. I.e., what do you THINK you can accomplish before the final project's deadline?
+
+TODO
+II would like to offer additional customization options for password generation, and deploy the web application on a cloud-based platform for scalability and availability.
+
+### In a sentence (or list of features), define a BEST outcome for your final project. I.e., what do you HOPE to accomplish before the final project's deadline?
+
+TODO
+
+## In a paragraph or more, outline your next steps. What new skills will you need to acquire? What topics will you need to research? If working with one of two classmates, who will do what?
+
+TODO
+Firstly, I will need to implement password hashing and salting to improve the security of the application. This will require me to acquire a deeper understanding of cryptography and hashing algorithms
+Secondly, I will focus on improving the front-end design of the application to make it more visually appealing and user-friendly. This will require me to learn more about web design and development
+Thirdly, I will work on additional customization options for password generation, such as allowing users to specify certain words or phrases to include in the password. 
